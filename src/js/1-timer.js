0a698b92e7bd181a976a63a02862a5002a3ed295
sashok1987nik <sashok1987nk@ukr.net>
2024-04-03 20:21:58 +0200

    timerDone

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,88 @@
-// Описаний в документації
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
-// Описаний у документації
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+const inputfield = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+
+let userSelectedDate = 0;
+
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    inputfield.disabled = true;
+    const timerID = setInterval(() => {
+        const countDown = userSelectedDate - Date.now();
+        if (countDown < 0) {
+            clearInterval(timerID);
+            inputfield.disabled = false;
+            
+        } else {
+            timer(convertMs(countDown));
+        }
+    }, 1000);
+
+});
+
+startBtn.disabled = true;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+    onClose(selectedDates) {
+        const currentDate = new Date();
+        if (selectedDates[0] - currentDate > 0) {
+          startBtn.disabled = false;
+        } else {
+          startBtn.disabled = true;
+    
+          iziToast.error({
+            message: 'Please choose a date in the future!',
+            position: 'topCenter', 
+            backgroundColor: '#ef4040',  
+            titleColor: '#FFFFFF',
+            messageColor: '#FFFFFF',
+            theme: 'dark',
+          });
+        }
+        userSelectedDate = selectedDates[0];
+      },
+  };
+
+  
+
+  flatpickr('#datetime-picker', options);
+
+  function timer({days, hours, minutes, seconds}) {
+    dataDays.textContent = addLeadingZero(days);
+    dataHours.textContent = addLeadingZero(hours);
+    dataMinutes.textContent = addLeadingZero(minutes);
+    dataSeconds.textContent = addLeadingZero(seconds);
+  }
+
+  function addLeadingZero(value) {
+    return String(value).padStart(2, 0);
+  }
+
+  function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
