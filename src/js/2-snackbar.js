3b98bb05db517e2285128400f41e2983340ebe04
sashok1987nik <sashok1987nk@ukr.net>
2024-04-05 14:38:55 +0200

    upSnackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,32 @@
-// Описаний у документації
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const delay = event.target.elements.delay.value;
+    const state = event.target.elements.state.value;
+
+
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(`✅ Fulfilled promise in ${delay}ms`);
+            } else {
+                reject(`❌ Rejected promise in ${delay}ms`);
+            }
+        }, delay);
+    })
+    .then(message => {
+        iziToast.success({
+            title: 'Success',
+            message: message,
+        });
+    })
+    .catch(message => {
+        iziToast.error({
+            title: 'Error',
+            message: message,
+        });
+    });
+})
